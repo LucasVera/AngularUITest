54f82595592001e1dadb0efce5531d4fe4d1f07b
Lucas <lucazzzv89@gmail.com>
2017-05-24 09:07:45 -0500

    dropdown in ui-grid

--- AngularUITest/App/controllers/gridCtrl.js
+++ AngularUITest/App/controllers/gridCtrl.js
@@ -1,7 +1,18 @@
 ﻿'use strict';
 
+var companyHash = {
+    1: 'Navitrans',
+    2: 'Blisoft'
+};
+
+var companyArray = [
+    0,
+    'Navitrans',
+    'Blisoft'
+];
+
 angular.module('AngularUI')
-.controller('GridCtrl', ['$scope', '$window', '$timeout', '$q', 'growl', function ($scope, $window, $timeout, $q, growl) {
+.controller('GridCtrl', ['$scope', '$window', '$timeout', '$q', '$uibModal', '$location', 'growl', function ($scope, $window, $timeout, $q, $uibModal, $location, growl) {
 
     $scope.initController = function () {
         $scope.users = [
@@ -40,8 +51,13 @@ angular.module('AngularUI')
                         { id: 2, company: 'Blisoft' }
                     ]
                 },
-                { name: 'Admin', field: 'isAdmin', enableCellEdit:false, type:'boolean', enableFiltering:false, minWidth:80 },
-                { name: 'Edad', field: 'age', type: 'number', enableFiltering:false, minWidth:80 },
+                { name: 'Admin', field: 'isAdmin', enableCellEdit:false, type:'boolean', enableFiltering:false, minWidth:80, allowCellFocus:false },
+                { name: 'Edad', field: 'age', type: 'number', enableFiltering: false, minWidth: 80 },
+                {
+                    name: 'Opciones', field: 'options', enableSorting:false, allowCellFocus:false, scope:$scope,
+                    cellTemplate: 'views/cellTemplates/gridOptionCellTemplate.html',
+                    enableCellEdit: false, enableFiltering: false, enableHiding: false, enableColumnResizing: false
+                }
             ],
             data: $scope.users,
             enableGridMenu: true,
@@ -50,7 +66,8 @@ angular.module('AngularUI')
                     title: 'Acción: refrescar datos',
                     action: function () {
                         //$window.alert('Datos renovados');
-                        growl.info('Datos renovados');
+                        growl.info('Cargando... ', {ttl:2100, onlyUniqueMessages: false});
+                        $timeout(function () { growl.success('Datos renovados'); }, 2000);
                     }
                 }
             ],
@@ -58,12 +75,12 @@ angular.module('AngularUI')
             onRegisterApi: function (gridApi) {
                 $scope.gridApi = gridApi;
                 $timeout(function () {
+                    // register grid callbacks
                     gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+                    gridApi.cellNav.on.navigate($scope, $scope.cellNavigated);
                 });
             }
         };
-
-
     }
 
     $scope.saveRow = function (rowEntity) {
@@ -73,7 +90,7 @@ angular.module('AngularUI')
 
         // fake a delay of 3 seconds. if Compañia is not blisoft, return error
         $timeout(function () {
-            if (rowEntity.company.toLowerCase() !== 'blisoft') {
+            if (companyArray[rowEntity.company].toLowerCase() !== 'blisoft') {
                 promise.reject();
                 growl.error(rowEntity.names + ' Debe tener como compañia Blisoft.');
             }
@@ -84,9 +101,32 @@ angular.module('AngularUI')
         }, 3000);
     }
 
-    $scope.doAction1 = function () {
-        //$window.alert('Acción 1 realizada!!');
-        growl.info('Scción 1 realizada');
+    $scope.cellNavigated = function (newRowCol, oldRowCol) {
+        if (newRowCol.col.field == 'options') {
+            $scope.selectedRow = newRowCol.row.entity;
+            $scope.dropdownIsOpen = true;
+            console.log($scope.gridOpts.appScope);
+        }
+    }
+
+    $scope.rowOption = function (optionClicked, row) {
+        switch (optionClicked) {
+            case 1: //change age to 99
+                row.age = 99;
+                break;
+
+            case 2: // mostrar nombre y apellidos en info
+                growl.warning(row.names + ' ' + row.lastNames);
+                break;
+
+            case 3: // ir a calendario
+                $location.path('/calendar');
+                break;
+
+            default:
+                console.log('Opción erronea...');
+                break;
+        }
     }
 
     $scope.addRecord = function (newRecord) {
@@ -99,10 +139,6 @@ angular.module('AngularUI')
     $scope.initController();
 }])
 .filter('mapCompany', function () {
-    var companyHash = {
-        1: 'Navitrans',
-        2: 'Blisoft'
-    };
 
     return function (input) {
         if (!input) {
